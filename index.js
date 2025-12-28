a8339775932b9f6d39ef8a168c5e3222f3aa736f
NikiforDragov <ndragov99@gmail.com>
2025-12-28 12:59:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScore = 0;
+let guestScore = 0;
+
+let homeScoreEl = document.getElementById('home-score');
+homeScoreEl.textContent = homeScore;
+
+let guestScoreEl = document.getElementById('guest-score');
+guestScoreEl.textContent = guestScore;
+
+function add1Home() {
+    homeScore += 1;
+    homeScoreEl.textContent = homeScore;
+}
+function add2Home() {
+    homeScore += 2;
+    homeScoreEl.textContent = homeScore;
+}
+function add3Home() {
+    homeScore += 3;
+    homeScoreEl.textContent = homeScore;
+}
+
+function add1Guest() {
+    guestScore += 1;
+    guestScoreEl.textContent = guestScore;
+}
+function add2Guest() {
+    guestScore += 2;
+    guestScoreEl.textContent = guestScore;
+}
+function add3Guest() {
+    guestScore += 3;
+    guestScoreEl.textContent = guestScore;
+}
+
+function newGame() {
+    homeScore = 0;
+    guestScore = 0;
+
+    homeScoreEl.textContent = homeScore;
+    guestScoreEl.textContent = guestScore;
+}
